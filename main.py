805225fabb0bb859b639240b9e58d1b30c238a19
Aderox <aderoxgame@gmail.com>
2022-07-02 18:35:17 +0200

    erk numpy i'll do my own matrice class

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from copyreg import constructor
 import matplotlib.pyplot as plt
 import numpy as np
 from matrice import *
-
+import math
 
 class Reseau:
     def __init__(self, activations, couchesIntermediaire, nbParCoucheInter, resultats):
@@ -24,8 +23,8 @@ class Reseau:
         self.poids = []
         self.biais = []
 
-        self.poids.append(np.random.randn(self.activations, self.nbParCoucheInter))
-        self.biais.append(np.random.randn(self.activations, 1))
+        self.poids.append(np.random.randn(self.nbParCoucheInter, self.activations)) #L'erreur est ici dans la formes des matrices
+        self.biais.append(np.zeros((self.activations, 1)))
         for i in range(1, self.COUCHE_TOTAL-1):
             self.poids.append(np.random.randn(self.nbParCoucheInter, self.nbParCoucheInter))
             self.biais.append(np.random.randn(self.nbParCoucheInter, 1))
@@ -37,7 +36,12 @@ class Reseau:
         self.Y = Y
 
     def calculateNextLayer(self, layer, X):
-        return np.dot(X, self.poids[layer]) + self.biais[layer]
+        produit = np.dot(self.poids[layer], X)
+        somme = produit
+        if layer < self.COUCHE_TOTAL-1:
+            somme = np.add(produit, self.biais[layer+1])
+        somme = self.sigmoid(somme)
+        return somme
 
     def show(self):
         print("Couche d'activation: " + str(self.activations))
@@ -55,14 +59,48 @@ class Reseau:
         print("Poids de couche de sortie: \n" + str(self.poids[len(self.poids)-1]) )
 
     def printBiais(self):
-        print("Biais d'activation: \n" + str(self.biais[0]) + '\n')
+        print("Biais d'activation (toujours nuls): \n" + str(self.biais[0]) + '\n')
         for i in range(1, self.couchesIntermediaire+1):
             print("Biais de couche intermédiaire {}: \n".format(i) + str(self.biais[i]) + '\n')
         print("Biais de couche de sortie: \n" + str(self.biais[len(self.biais)-1]))
 
+
+    def subtractMatrice(self, A, B):
+        n = 0
+        result = []
+        for cell in A.flatten():
+            result.append(cell - B[n])
+        return result
+
+    def guess(self, X):
+        for i in range(0, self.COUCHE_TOTAL):
+            X = self.sigmoid(self.calculateNextLayer(i, X))
+        return X
+
+    def sigmoid(self, X):
+        #f(x)=1/1+e-x
+        s = lambda x : 1 / (1+math.exp(-x))
+        sV = np.vectorize(s)
+        return sV(X)
+    
+    def cost(self, X, Y):
+        guessed = self.guess(X)
+        print("guessed: " + str(guessed))
+        print("Y: " + str(Y))
+        ecart = self.subtractMatrice(guessed, Y)
+        print("ecart: " + str(ecart))
+        squareMatrice = np.vectorize(lambda x : x**2)
+        ecartSquared = squareMatrice(ecart)
+        return ecartSquared.sum()
+
 def main():
     reseau = Reseau(3, 2, 2, 1)
     reseau.show()
+    reseau.setTrainingData([[0.4],[0.32],[0.876]],[[0,3333]])
+    print(reseau.cost([[0.4],[0.32],[0.876]],[[0,3333]]))
+    matriceA = Matrice(2,2,[1,2,3,4])
+    matriceB = Matrice(2, 1, [5,6])
+    print(matriceA.multiply(matriceB))
 
 if __name__ == "__main__":
     main()
--- matrice.py
+++ matrice.py
@@ -1,13 +1,72 @@
-@DeprecationWarning
+'''
+Une matrice (i,j) avec i le nombre de ligne et j le nombre de colonne
+Soit Index
+x = index%colonne
+y = index/colonne
+
+Donc:
+index congru x modulo j
+colonne * y + x  = index 
+SSI CA COMMENCE EN (0,0)
+'''
 class Matrice:
     def __init__(self, lignes, colones, valeurs = []) -> None:
         self.lignes = lignes
         self.colones = colones
         self.valeurs = valeurs
 
+    def multiply(self, B):
+        if self.colones != B.getLines():
+            raise Exception("Erreur: Les deux matrices ne peuvent pas être multipliées.")
+        resultat = self.initList(self.lignes * B.getColones())
+        matriceResultat = Matrice(self.lignes, B.getColones(), resultat)
+        for h in range(0, self.lignes):
+            for i in range(0, B.getColones()):
+                sum = 0
+                for j in range(0, self.colones):
+                    sum += self.valeurs[j+h*self.colones] * B.getValue(i, j)
+                matriceResultat.setValue(sum, i, h)           
+        return matriceResultat
 
+    def add(self, B):
+        if self.colones != B.getColones() or self.lignes != B.getLines():
+            raise Exception("Erreur: impossible d'ajouter les deux matrices")
+        resultat = self.initList(self.lignes * self.colones)
+        matriceResultat = Matrice(self.lignes, self.colones, resultat)
+        for i in range(0, self.lignes * self.colones):
+            resultat[i] = self.valeurs[i] + B.getValueByIndex(i)
+        return matriceResultat
+    
+    def applyFunc(self, fun):
+        for i in range(0, len(self.valeurs)):
+            self.valeurs[i] = fun(self.valeurs[i])
+
+    def initList(self, size):
+        liste = []
+        for i in range(0, size):
+            liste.append(0)
+        return liste
+
+    def setValue(self, value, x, y):
+        self.setValueByIndex(value, y * self.colones + x)
+
+    def setValueByIndex(self, value, index):
+        self.valeurs[index] = value
+
+    def getValueByIndex(self, index):
+        return self.valeurs[index]
+    def getValue(self, x,y):
+        return self.getValueByIndex(self.colones * y + x)
+
+    def getCoordonate(index):
+        return (index%self.colones, index//self.colones)
+
+    def getLines(self):
+        return self.lignes
+    def getColones(self):
+        return self.colones
 
     def __str__(self) -> str:
-        return str(self.valeurs)
+        return "Lignes: {} Colonnes {} Valeurs: ".format(self.lignes, self.colones) + str(self.valeurs)
 
     
